0f5dec9c5c94a7f710f7f448a144bf16a5504152
Bineesh-pro <bineeshprofessional@gmail.com>
2024-06-17 16:00:36 +0530

    initial commit

---
+++ src/main/java/com/bineesh/makersharksassignment/MakersharksassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.bineesh.makersharksassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakersharksassignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakersharksassignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bineesh/makersharksassignment/MakersharksassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bineesh.makersharksassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakersharksassignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
